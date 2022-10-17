edf166bdc46900714745a7224c943c43372d8b71
dev-yakuza <dev.yakuza@gmail.com>
2022-10-17 21:39:26 +0900

    refactor: Use MUI styled instead of Emtion styled

--- src/components/organisms/BlogItem/index.tsx
+++ src/components/organisms/BlogItem/index.tsx
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled'
 import { Grid, Typography } from '@mui/material'
+import { styled } from '@mui/system'
 
 const Container = styled(Grid)`
   background-color: #fff;
