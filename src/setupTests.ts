d8b931065047686a71692f54601cb2d179730dbc
dev-yakuza <dev.yakuza@gmail.com>
2022-10-17 21:57:39 +0900

    test: Add Emotion serializer for checking CSS

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+import { matchers, createSerializer } from '@emotion/jest'
+expect.extend(matchers)
+expect.addSnapshotSerializer(createSerializer())
+
+module.exports = {
+  snapshotSerializers: ['@emotion/jest/serializer'],
+}
