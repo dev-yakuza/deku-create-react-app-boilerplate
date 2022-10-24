920ab42ffc437c925b65322319be4bcc9363ab56
JeongHean Kim <dev.yakuza@gmail.com>
2022-10-24 22:00:26 +0900

    feat: Add getting Posts API (#221)

---
+++ src/api/posts/index.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import type { Post } from 'types'
+
+const POSTS_URL = `${process.env.REACT_APP_API_SERVER}/posts`
+
+const getPosts = async (): Promise<ReadonlyArray<Post>> => {
+  const response = await axios.get(POSTS_URL)
+
+  return response.data
+}
+
+export const useGetPosts = () => {
+  return useQuery(['GetPosts'], async () => getPosts(), {
+    refetchOnWindowFocus: false,
+  })
+}
