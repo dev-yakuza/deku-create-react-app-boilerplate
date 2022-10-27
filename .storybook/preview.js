e4d84d40c7a87d59777bb202efa2158eda2eb0f5
JeongHean Kim <dev.yakuza@gmail.com>
2022-10-27 21:53:16 +0900

    feat: Add blog list page (#228)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from '@mui/material'
 import { withRouter } from 'storybook-addon-react-router-v6'
 import { theme } from '../src/utils/theme'
@@ -12,11 +13,22 @@ export const parameters = {
   },
   layout: 'fullscreen',
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
+
 export const decorators = [
   (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    </QueryClientProvider>
   ),
   withRouter,
 ]
---
+++ src/components/pages/BlogListPage/index.test.tsx
@@ -0,0 +1,49 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import mockPostsData from 'api/posts/mockData/posts.json'
+import { BrowserRouter } from 'react-router-dom'
+import type { Post } from 'types'
+import { queryClient, mockBlogList, mockPageLoading } from 'utils/test'
+
+import { BlogListPage } from '.'
+
+let mockResponse = {
+  data: [] as readonly Post[],
+  isLoading: true,
+}
+jest.mock('api/posts', () => ({ useGetPosts: () => mockResponse }))
+
+describe('BlogListPage', () => {
+  it('rendered well with data', () => {
+    mockResponse = { data: mockPostsData, isLoading: false }
+
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <BlogListPage />
+        </BrowserRouter>
+      </QueryClientProvider>,
+    )
+
+    expect(mockBlogList.mock.calls.length).toBe(1)
+    expect(mockBlogList.mock.calls[0][0].posts).toBe(mockPostsData)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('show loading when data is loading', () => {
+    mockResponse = { data: [], isLoading: true }
+
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <BlogListPage />
+        </BrowserRouter>
+      </QueryClientProvider>,
+    )
+
+    expect(mockPageLoading.mock.calls.length).toBe(1)
+
+    expect(container).toMatchSnapshot()
+  })
+})
